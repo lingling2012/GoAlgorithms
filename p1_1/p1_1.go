4092a7e3da224bbb1365fd7f345ef5db6511429f
rainliu <yliu@live.com>
2013-07-27 00:41:07 -0700

    update code

--- p1_1/p1_1.go
+++ p1_1/p1_1.go
@@ -4,38 +4,44 @@ import (
 	"fmt"
 )
 
-func FastFind(cin [][2]int, N int) {
-	var p, q int
-	var id []int
-
+func FastFind(cin [][2]int, N int) {	
+	id := make([]int, N)
+	
 	fmt.Printf("(p q):  ")
-
-	id = make([]int, N)
 	for i := 0; i < N; i++ {
 		id[i] = i
 		fmt.Printf("%d ", id[i])
 	}
-	fmt.Printf("\n\n")
+	fmt.Printf(": (id_r id_w)\n\n")
 
 	for l := 0; l < len(cin); l++ {
-		p = cin[l][0]
-		q = cin[l][1]
+		p := cin[l][0]
+		q := cin[l][1]
 
+		id_r := 0;
+		id_w := 0;
 //////////////////////////////////////
 		t := id[p]
+		id_r++;
 		if t != id[q] {
 			for i := 0; i < N; i++ {
 				if id[i] == t {
 					id[i] = id[q]
+					id_r++;
+					id_w++;
 				}
+				id_r++;
 			}
 		}
+		id_r++;
 //////////////////////////////////////
 		
 		fmt.Printf("(%d %d):  ", p, q)
 		for k := 0; k < N; k++ {
 			fmt.Printf("%d ", id[k])
 		}
+		
+		fmt.Printf(": (%4d %4d) ", id_r, id_w);
 		
 		if t != id[q] {
 			fmt.Printf(":  (%d %d) \n", p, q);
--- p1_2/p1_2.go
+++ p1_2/p1_2.go
@@ -5,30 +5,34 @@ import (
 )
 
 func FastUnion(cin [][2]int, N int) {
-	var p, q int
-	var id []int
-
+	id := make([]int, N)
+	
 	fmt.Printf("(p q):  ")
-
-	id = make([]int, N)
 	for i := 0; i < N; i++ {
 		id[i] = i
 		fmt.Printf("%d ", id[i])
 	}
-	fmt.Printf("\n\n")
+	fmt.Printf(": (id_r id_w)\n\n")
 
 	for l := 0; l < len(cin); l++ {
-		p = cin[l][0]
-		q = cin[l][1]
+		p := cin[l][0]
+		q := cin[l][1]
 		
+		id_r := 0;
+		id_w := 0;
 //////////////////////////////////////
 		var i, j int;
-		for i=p; i!=id[i]; i=id[i] {
+		for i=p; i!=id[i]; i, id_r =id[i], id_r+1 {
+			id_r++;
 		}
-		for j=q; j!=id[j]; j=id[j] {
+		id_r++;
+		for j=q; j!=id[j]; j, id_r =id[j], id_r+1 {
+			id_r++;
 		}
+		id_r++
 		if i!=j {
 			id[i] = j;
+			id_w++;
 		}
 //////////////////////////////////////
 
@@ -36,6 +40,8 @@ func FastUnion(cin [][2]int, N int) {
 		for k := 0; k < N; k++ {
 			fmt.Printf("%d ", id[k])
 		}
+		
+		fmt.Printf(": (%4d %4d) ", id_r, id_w);
 		
 		if i != j {
 			fmt.Printf(":  (%d %d) \n", p, q);
