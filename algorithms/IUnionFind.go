257527fd41f181bb6db7f5e238958f0c8de2108f
rainliu <yliu@live.com>
2013-09-21 00:40:25 -0700

    add unionFind

---
+++ algorithms/IUnionFind.go
@@ -0,0 +1,15 @@
+package algorithms
+
+import ()
+
+type VUnionFind interface { //Virtual Part of UnionFind Interface
+	Union(p, q int)
+	Find(p int) int
+}
+
+type IUnionFind interface {
+	VUnionFind
+	Connected(p, q int) bool
+	Count() int
+	Summary()
+}
---
+++ algorithms/WeightedQuickUnion_test.go
@@ -0,0 +1,42 @@
+package algorithms
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestWeightedQuickUnion(t *testing.T) {
+	const N = 10
+	var cin = [][2]int{
+		{4, 3},
+		{3, 8},
+		{6, 5},
+		{9, 4},
+		{2, 1},
+		{8, 9},
+		{5, 0},
+		{7, 2},
+		{6, 1},
+		{1, 0},
+		{6, 7},
+	}
+	var uf IUnionFind
+	
+	uf = NewWeightedQuickUnion(N)
+	for i := 0; i < len(cin); i++ {
+		p := cin[i][0]
+		q := cin[i][1]
+		
+		uf.Summary();
+		fmt.Printf("\n");
+		
+		if uf.Connected(p, q) {
+			continue
+		}
+		uf.Union(p, q)
+
+		uf.Summary();
+		fmt.Printf("%d %d\n", p, q)
+	}
+	fmt.Printf("%d components\n", uf.Count())
+}
