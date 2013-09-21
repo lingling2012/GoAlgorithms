4a5b92e7806532d9abfb206f4b0523378dd3d1fe
rainliu <yliu@live.com>
2013-09-20 22:53:08 -0700

    add linked_list implementation

--- algorithms/linked_list.go
+++ algorithms/linked_list.go
@@ -1,6 +1,56 @@
 package algorithms
 
 import (
-
+	"fmt"
 )
 
+type LinkedElement struct{
+	key  int;
+	data interface{}
+	
+	prev *LinkedElement;
+	next *LinkedElement;
+}
+
+type LinkedList struct{
+	sentinel *LinkedElement;
+}
+
+func NewLinkedList() *LinkedList{
+	this := &LinkedList{};
+	this.sentinel = &LinkedElement{};
+	this.sentinel.prev = this.sentinel;
+	this.sentinel.next = this.sentinel;
+	return this;
+}
+
+func (this *LinkedList) Insert(x *LinkedElement){
+	x.next = this.sentinel.next;
+	this.sentinel.next.prev = x;
+	this.sentinel.next = x;
+	x.prev = this.sentinel;
+}
+
+func (this *LinkedList) Delete(x *LinkedElement){
+	x.prev.next = x.next;
+	x.next.prev = x.prev;	
+}
+
+func (this *LinkedList) Search(k int) *LinkedElement{
+	x := this.sentinel.next;
+	for x!=this.sentinel && x.key != k{
+		x = x.next;
+	}
+	
+	return x;
+}
+
+func (this *LinkedList) Summary(){
+	x := this.sentinel.next;
+	fmt.Printf("nil");
+	for x!=this.sentinel{
+		fmt.Printf("->%d", x.key);
+		x = x.next;
+	}
+	fmt.Printf("\n");
+}
---
+++ algorithms/linked_list_test.go
@@ -0,0 +1,22 @@
+package algorithms
+
+import (
+    "testing"
+    //"fmt"
+)
+
+func TestLinkedList(t *testing.T) {
+	var A =[]int{1, 4, 16, 9, 25};
+	
+	l := NewLinkedList();
+	
+	for i:=0; i<len(A); i++{
+		x := &LinkedElement{A[i], A[i], nil, nil};
+		l.Insert(x);
+		l.Summary();
+	}
+	x := l.Search(1);
+	l.Delete(x);
+	
+	l.Summary();
+}
